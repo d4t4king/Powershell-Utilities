a5083a50961057d41e035c05c54f67cb47d9d000
Charlie Heselton <cheselton@costar.com>
2026-01-20 11:02:14 -0500

    Initial commit for this file.

---
+++ Get-OSVersion.ps1
@@ -0,0 +1,26 @@
+<#
+
+    .SYNOPSIS
+    Gets the OS version from the kernel32.dll.
+
+#>
+
+function Get-OSVersion
+{
+    $signature = @"
+    [DllImport("kernel32.dll")]
+    public static extern uint GetVersion();
+"@
+Add-Type -MemberDefinition $signature -Name "Win32OSVersion" -Namespace Win32Funtions -PassThru
+}
+
+$os = [System.BitConverter]::GetBytes((Get-OSVersion)::GetVersion())
+$majorVersion = $os[0]
+$minorVersion = $os[1]
+$build = [byte]$os[2],[byte]$os[3]
+$buildNumber = [System.BitConverter]::ToInt16($build,0)
+
+$osversion = [environment]::OSVersion
+
+"Version is {0}.{1} build {2}" -F $majorVersion,$minorVersion,$buildNumber
+Write-Output "Compare to: $osversion"
